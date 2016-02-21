9619615af5d10f811961bb1a3d32fbc86a77d7a3
Simon Degraeve <simon.degraeve@gmail.com>
2016-02-21 17:51:04 +0800

    refactor: remove business logic from index

---
+++ src/preset.js
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+export default {
+  plugins: [
+    'add-module-exports',
+    'transform-es2015-destructuring',
+    'transform-es2015-modules-commonjs',
+    'transform-es2015-parameters',
+    'transform-es2015-sticky-regex',
+    'transform-es2015-unicode-regex',
+    'transform-strict-mode',
+    'transform-flow-strip-types',
+    'transform-object-rest-spread',
+    'transform-class-properties',
+    'transform-decorators',
+    'transform-async-to-generator'
+  ]
+}
